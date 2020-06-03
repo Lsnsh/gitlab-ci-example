1ad446c1650c34d43356e5ef8cdda45d0bbd1bed
Lsnsh <zehexin6@163.com>
2020-06-03 20:24:01 +0800

    script: check the directory exists before copy

--- build/deploy-test.sh
+++ build/deploy-test.sh
@@ -1,1 +1,7 @@
-cp -rf dist/* /www/test/gitlab-ci-example
+# 如果目录不存在，则创建目录，否则拷贝构建好的文件到该目录
+if [ ! -d /www/test/gitlab-ci-example  ];then
+  mkdir /www/test/gitlab-ci-example
+else
+  echo dir exist
+  cp -rf dist/* /www/test/gitlab-ci-example
+fi
