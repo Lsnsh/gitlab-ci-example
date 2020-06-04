b0889d65f7ec1554b367f817d0308a2e6ccec9cf
Lsnsh <zehexin6@163.com>
2020-06-04 12:17:06 +0800

    script: (deploy-test) print prompt message when no such directory

--- build/deploy-test.sh
+++ build/deploy-test.sh
@@ -1,7 +1,9 @@
-# 如果目录不存在，则创建目录，否则拷贝构建好的文件到该目录
-if [ ! -d /www/test/gitlab-ci-example  ];then
-  mkdir /www/test/gitlab-ci-example
+# 如果目录不存在，则提示目录不存在并引导创建，否则拷贝构建好的文件到该目录
+TEST_PATH="/www/test/gitlab-ci-example";
+if [ ! -d $TEST_PATH  ];then
+  echo "$TEST_PATH: No such file or directory"
+  echo "Please create this directory and then assign the directory permissions to the gitlab-runner user.\nYou can execute the following command as root:"
+  echo "mkdir $TEST_PATH\nchown gitlab-runner $TEST_PATH"
 else
-  echo dir exist
-  cp -rf dist/* /www/test/gitlab-ci-example
+  cp -rf dist/* $TEST_PATH
 fi
